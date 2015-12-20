6e86f3c484eda4a464413a0c19493d7914b48da8
Alejandro Pérez Martín <aleperez92@gmail.com>
2015-12-20 02:06:55 +0000

    Added week 2 homework

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -64,9 +64,131 @@ app.controller('menuController', ['$scope', function ($scope) {
         return $scope.tab === tabIndex;
     };
 
-    $scope.toggleDetails = function(){
+    $scope.toggleDetails = function () {
         $scope.showDetails = !$scope.showDetails;
     };
 
 
+}]);
+
+app.controller('ContactController', ['$scope', function ($scope) {
+
+    $scope.feedback = {
+        mychannel: "",
+        firstName: "",
+        lastName: "",
+        agree: false,
+        email: ""
+    };
+
+    var channels = [{
+        value: "tel",
+        label: "Tel."
+    }, {
+        value: "Email",
+        label: "Email"
+    }];
+
+    $scope.channels = channels;
+    $scope.invalidChannelSelection = false;
+
+}]);
+
+app.controller('FeedbackController', ['$scope', function ($scope) {
+
+    $scope.sendFeedback = function () {
+
+        console.log($scope.feedback);
+
+        if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+            $scope.invalidChannelSelection = true;
+            console.log('incorrect');
+        } else {
+            $scope.invalidChannelSelection = false;
+            $scope.feedback = {
+                mychannel: "",
+                firstName: "",
+                lastName: "",
+                agree: false,
+                email: ""
+            };
+            $scope.feedback.mychannel = "";
+
+            $scope.feedbackForm.$setPristine();
+            console.log($scope.feedback);
+        }
+    };
+
+}]);
+
+app.controller('DishDetailController', ['$scope', function ($scope) {
+
+    var dish = {
+        name: 'Uthapizza',
+        image: 'images/uthapizza.png',
+        category: 'mains',
+        label: 'Hot',
+        price: '4.99',
+        description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+        comments: [{
+                rating: 5,
+                comment: "Imagine all the eatables, living in conFusion!",
+                author: "John Lemon",
+                date: "2012-10-16T17:57:28.556094Z"
+            }, {
+                rating: 4,
+                comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                author: "Paul McVites",
+                date: "2014-09-05T17:57:28.556094Z"
+            }, {
+                rating: 3,
+                comment: "Eat it, just eat it!",
+                author: "Michael Jaikishan",
+                date: "2015-02-13T17:57:28.556094Z"
+            }, {
+                rating: 4,
+                comment: "Ultimate, Reaching for the stars!",
+                author: "Ringo Starry",
+                date: "2013-12-02T17:57:28.556094Z"
+            }, {
+                rating: 2,
+                comment: "It's your birthday, we're gonna party!",
+                author: "25 Cent",
+                date: "2011-12-02T17:57:28.556094Z"
+            }
+
+        ]
+    };
+
+    $scope.dish = dish;
+
+}]);
+
+app.controller('DishCommentController', ['$scope', function ($scope) {
+
+    //Step 1: Create a JavaScript object to hold the comment from the form
+    $scope.comment = {
+        rating: 5,
+        author: "",
+        comment: "",
+        date: ""
+    };
+
+    $scope.submitComment = function () {
+
+        $scope.comment.date = new Date().toISOString();
+
+        console.log($scope.comment);
+
+        $scope.dish.comments.push($scope.comment);
+
+        $scope.commentForm.$setPristine();
+
+        $scope.comment = {
+            rating: 5,
+            author: "",
+            comment: "",
+            date: ""
+        };
+    }
 }]);
