de7e6a13a8213df8f7dd44acd05bbf126d2e3890
Alejandro Pérez Martín <aleperez92@gmail.com>
2016-01-28 19:20:21 +0100

    Added  to use REST service

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -8,7 +8,12 @@ angular.module('confusionApp')
             $scope.filtText = '';
             $scope.showDetails = false;
 
-            $scope.dishes= menuFactory.getDishes();
+            $scope.dishes = {};
+
+            menuFactory.getDishes()
+                .then(function(response){
+                    $scope.dishes = response.data;
+            });
 
             $scope.select = function(setTab) {
                 $scope.tab = setTab;
@@ -69,9 +74,15 @@ angular.module('confusionApp')
 
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
-            var dish= menuFactory.getDish(parseInt($stateParams.id,10));
+            $scope.dish = {};
 
-            $scope.dish = dish;
+            menuFactory.getDish(parseInt($stateParams.id,10))
+            .then(
+                function(response){
+                    $scope.dish = response.data;
+                    $scope.showDish=true;
+                }
+            );
 
         }])
 
@@ -95,7 +106,13 @@ angular.module('confusionApp')
         .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory) {
 
             $scope.specialist = corporateFactory.getLeader(3);
-            $scope.featured = menuFactory.getDish(0);
+            $scope.featured = {};
+
+            menuFactory.getDish(0)
+                .then(function(response){
+                    $scope.featured = response.data;
+            });
+
             $scope.promotion = menuFactory.getPromotion(0);
 
         }])
